810902bb319bacd080cfba1d16d22bb49ac861a4
Bibhabasu Mohapatra <68384968+bibhabasumohapatra@users.noreply.github.com>
2021-12-22 10:01:11 +0530

    taken from abhishek's MLframework
    rather writing catfeats module for now taken code for abhishek's https://github.com/abhishekkrthakur/mlframework/blob/master/src/categorical.py

--- create_csv.py
+++ create_csv.py
@@ -1,22 +1,87 @@
-import sklearn
-import pandas as pd
-class preprocessing:
-    def __init__(self, prob_type, train_path, test_path):
-        '''
-        prob_type(str) : 'regression', binary', 'multiclass'
-        train_path(str): path of the train csv file, for example : "C:\Users\Desktop\df_train.csv"
-        test_path(str):  path of the test csv file, for example : "C:\Users\Desktop\df_test.csv"
+'''
+From abhishek Thakur https://github.com/abhishekkrthakur/mlframework/blob/master/src/categorical.py
+'''
+from sklearn import preprocessing
 
-        '''
-        self.prob_type = prob_type
-        self.train_path = train_path 
-        self.test_path = test_path
-        self.df = pd.read_csv(self.train_path)
-        self.cat_feats = self.df.select_dtypes(include=['object','str']).columns.tolist() 
+class CategoricalFeatures:
+    def __init__(self, df, categorical_features, encoding_type, handle_na=False):
+        """
+        df: pandas dataframe
+        categorical_features: list of column names, e.g. ["ord_1", "nom_0"......]
+        encoding_type: label, binary, ohe
+        handle_na: True/False
+        """
+        self.df = df
+        self.cat_feats = categorical_features
+        self.enc_type = encoding_type
+        self.handle_na = handle_na
+        self.label_encoders = dict()
+        self.binary_encoders = dict()
+        self.ohe = None
 
-    # Manage the missing values in df
-
-    # The functions for managing categorical features taken
-    # from https://github.com/abhishekkrthakur/mlframework/blob/master/src/categorical.py
-
+        if self.handle_na:
+            for c in self.cat_feats:
+                self.df.loc[:, c] = self.df.loc[:, c].astype(str).fillna("-9999999")
+        self.output_df = self.df.copy(deep=True)
     
+    def _label_encoding(self):
+        for c in self.cat_feats:
+            lbl = preprocessing.LabelEncoder()
+            lbl.fit(self.df[c].values)
+            self.output_df.loc[:, c] = lbl.transform(self.df[c].values)
+            self.label_encoders[c] = lbl
+        return self.output_df
+    
+    def _label_binarization(self):
+        for c in self.cat_feats:
+            lbl = preprocessing.LabelBinarizer()
+            lbl.fit(self.df[c].values)
+            val = lbl.transform(self.df[c].values)
+            self.output_df = self.output_df.drop(c, axis=1)
+            for j in range(val.shape[1]):
+                new_col_name = c + f"__bin_{j}"
+                self.output_df[new_col_name] = val[:, j]
+            self.binary_encoders[c] = lbl
+        return self.output_df
+
+    def _one_hot(self):
+        ohe = preprocessing.OneHotEncoder()
+        ohe.fit(self.df[self.cat_feats].values)
+        return ohe.transform(self.df[self.cat_feats].values)
+
+    def fit_transform(self):
+        if self.enc_type == "label":
+            return self._label_encoding()
+        elif self.enc_type == "binary":
+            return self._label_binarization()
+        elif self.enc_type == "ohe":
+            return self._one_hot()
+        else:
+            raise Exception("Encoding type not understood")
+    
+    def transform(self, dataframe):
+        if self.handle_na:
+            for c in self.cat_feats:
+                dataframe.loc[:, c] = dataframe.loc[:, c].astype(str).fillna("-9999999")
+
+        if self.enc_type == "label":
+            for c, lbl in self.label_encoders.items():
+                dataframe.loc[:, c] = lbl.transform(dataframe[c].values)
+            return dataframe
+
+        elif self.enc_type == "binary":
+            for c, lbl in self.binary_encoders.items():
+                val = lbl.transform(dataframe[c].values)
+                dataframe = dataframe.drop(c, axis=1)
+                
+                for j in range(val.shape[1]):
+                    new_col_name = c + f"__bin_{j}"
+                    dataframe[new_col_name] = val[:, j]
+            return dataframe
+
+        elif self.enc_type == "ohe":
+            return self.ohe(dataframe[self.cat_feats].values)
+        
+        else:
+            raise Exception("Encoding type not understood")
+                
